8e8c91ef58901e456df8ee17e560e0b42b0d0905
RaisaNuzhat <raisa.csecu@gmail.com>
2024-05-14 20:40:23 +0600

    updated

--- index.js
+++ index.js
@@ -12,12 +12,38 @@ const port = process.env.PORT || 5000
 //  middlewares
 const corsOptions =
 {
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173","https://th-assignment-376c0.web.app","https://th-assignment-376c0.firebaseapp.com"],
     credentials:true,
     optionsSuccessStatus:200,
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
+
+const verifyToken =  (req,res,next)=>
+  {
+      console.log("asire bhai asi")
+
+      const token = req.cookies?.token
+      if(!token) res.status(401).send({message:"unauthorized access"})
+            console.log(token)
+            if(token)
+              {
+                jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>
+                {
+                      if(err)
+                        {
+                          console.log(err)
+                          return res.status(401).send({message:"unauthorized access"})
+
+                        }
+                        console.log(decoded)
+                        req.user = decoded
+                        next()
+                })
+              }
+      
+  }
 
 const uri =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9odt6wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 //console.log(uri)
@@ -57,17 +83,36 @@ async function run() {
       ).send({success: true})
     })
      //clearing token
-     app.get('/logout',(req,res)=>
-    {
-      res.clearCookie('token',
-      {
-        httpOnly:true,
-        secure:process.env.NODE_ENV==='production',
-        sameSite:process.env.NODE_ENV==='production'?'none':'strict',
-        maxAge:0,
-      }
-    ).send({success: true})
-    })
+    //  app.get('/logout',(req,res)=>
+    // {
+    //   res.clearCookie('token',
+    //   {
+    //     httpOnly:true,
+    //     secure:process.env.NODE_ENV==='production',
+    //     sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+    //     maxAge:0,
+    //   }
+    // ).send({success: true})
+    // })
+      //clearing token
+      // app.get('/logout',(req,res)=>
+      //   {
+      //     res.clearCookie('token',
+      //     {
+      //       httpOnly:true,
+      //       secure:process.env.NODE_ENV==='production',
+      //       sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+      //       maxAge:0,
+      //     }
+      //   ).send({success: true})
+      //   })
+      app.post('/logout', async (req, res) => {
+        const user = req.body;
+        console.log('logging out', user);
+        res
+            .clearCookie('token', { maxAge: 0, sameSite: 'none', secure: true })
+            .send({ success: true })
+      })
     //get all volunteers
       app.get('/volunteers',async(req,res) =>
         {
@@ -146,17 +191,28 @@ async function run() {
           })
 
           // email filtering
-      app.get('/volunteer/:orgemail',async(req,res) =>
+      app.get('/volunteer/:orgemail',verifyToken,async(req,res) =>
         {
+            const tokenEmail = req.user.email
+            //console.log(tokenData,'from token')
             console.log(req.params.orgemail)
+            if(tokenEmail !== req.params.orgemail)
+              {
+                return res.status(403).send({message:"forbidden access"})
+              }
             const result = await volunteerCollection.find({orgemail: req.params.orgemail}).toArray()
             console.log(result)
             res.send(result)
         })
         //request email filtering
-        app.get('/request/:vemail',async(req,res) =>
+        app.get('/request/:vemail',verifyToken,async(req,res) =>
           {
+            const tokenEmail = req.user.email
               console.log(req.params.vemail)
+              if(tokenEmail !== req.params.vemail)
+                {
+                  return res.status(403).send({message:"forbidden access"})
+                }
               const result = await volunteerRequestCollection.find({vemail: req.params.vemail}).toArray()
               console.log(result)
               res.send(result)
